c9ec4bd484179da6a2f8204cdfe7b2af3aa8dd9d
Dhriti <dhritigabani@gmail.com>
2022-01-19 01:28:32 -0500

    update: title changes

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import Heart from "react-animated-heart";
 
 export default function Like({ isLiked, handleclick }) {
-  return (
-    <div onClick={() => onclick()}>
-      <Heart isClick={isLiked} onClick={handleclick} />
-    </div>
-  );
+  return <Heart isClick={isLiked} onClick={handleclick} />;
 }
